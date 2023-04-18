31edeed8c442bc35d5fa94251f6b8b2945b1da89
Floare George Octavian <tavi.latavi121@gmail.com>
2023-04-19 00:23:12 +0300

    partial same process thrad

--- a2/a2.c
+++ a2/a2.c
@@ -15,18 +15,76 @@
 #include <unistd.h>
 #include "a2_helper.h"
 
+int _t1_start_flag = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t done_condition = PTHREAD_COND_INITIALIZER;
+
+
+
+typedef struct {
+    int pid;
+    int tid;
+    pthread_mutex_t lock;
+} TH_INFO;
+
+//TODO: double conditional mutex to end t1 after t3
+
+void* function(void* param) {
+    TH_INFO* s = (TH_INFO*)param;
+    if (s->tid == 3) {
+        pthread_mutex_lock(&lock);
+        while (!_t1_start_flag) {
+            pthread_cond_wait(&done_condition, &lock);
+        }
+        pthread_mutex_unlock(&lock);
+    }
+    info(BEGIN, s->pid, s->tid);
+    if (s->tid == 1) {
+        pthread_mutex_lock(&lock);
+        _t1_start_flag = 1;
+        pthread_cond_signal(&done_condition);
+        pthread_mutex_unlock(&lock);
+    }
+    info(END, s->pid, s->tid);
+    return NULL;
+}
+
 int main() {
 
     pid_t pid2, pid3, pid4, pid5, pid6, pid7;
-
-    // printf("%d\n", getpid());
-
+    pthread_t tid[4] = { -1, -1, -1, -1};
+    pthread_mutex_t lock;
     init();
+
     info(BEGIN, 1, 0);
     pid2 = fork();
 
     if (pid2 == 0) {
         info(BEGIN, 2, 0);
+        //2.3
+        TH_INFO th_info[4];
+        if (pthread_mutex_init(&lock, NULL) != 0) {
+            return -1;
+        }
+        for (int i = 0; i < 4; i++) {
+            th_info[i].tid = i + 1;
+            th_info[i].pid = 2;
+            th_info[i].lock = lock;
+        }
+
+        for (int i = 1; i <= 4; i++) {
+            if (pthread_create(&tid[i - 1], NULL, function, &th_info[i - 1]) != 0) {
+                perror("Error creating thread");
+                return -1;
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (i == 0) {
+                pthread_join(tid[2], NULL);
+            }
+        }
+        pthread_mutex_destroy(&lock);
 
         pid3 = fork();
         if (pid3 == 0) {
@@ -47,7 +105,6 @@ int main() {
         }
         pid5 = fork();
         if (pid5 == 0) {
-            //P5 as child
             info(BEGIN, 5, 0);
             pid6 = fork();
             if (pid6 == 0) {
@@ -69,11 +126,6 @@ int main() {
         } else {
             wait(NULL);
         }
-
-
-
-
-
         info(END, 2, 0);
         exit(0);
     }
