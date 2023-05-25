ca269691873463bcc1d88fdf7367bb434620985d
Floare George Octavian <tavi.latavi121@gmail.com>
2023-05-25 10:27:36 +0300

    compilation error fixed

--- a3/a3.c
+++ a3/a3.c
@@ -10,7 +10,30 @@
 
 #define PIPE_write "RESP_PIPE_55635"
 #define PIPE_read "REQ_PIPE_55635"
+#define ShSIZE 1708215
 #define ShM "/55Meom"
+
+char* int_to_string(unsigned int val) {
+	char* result;
+
+	int size = 0;
+	int temp = val;
+	while (temp != 0) {
+		temp /= 10;
+		size++;
+	}
+
+	result = malloc((size + 1) * sizeof(char));
+
+	for (int i = 0; i < size; i++) {
+		int digit = val % 10;
+		val /= 10;
+		result[i] = (char)(digit + '0');
+	}
+	result[size] = '\0';
+	return result;
+
+}
 
 int main(int argc, char** argv) {
 
@@ -18,13 +41,15 @@ int main(int argc, char** argv) {
 	int fd_read = -1;
 	unsigned int version = 55635;
 	char variant[] = "VARIANT";
+	char map_file[] = "MAP_FILE";
+	char write_to_shm[] = "WRITE_TO_SHM";
 	char value[] = "VALUE";
 	char success[] = "SUCCESS";
 	char error[] = "ERROR";
 	unsigned int size_sh = 0;
 	volatile char* shared_mem = NULL;
 	unsigned int offset, val;
-	char file_name[];
+	char *file_name;
 	mkfifo(PIPE_write, 0600);
 
 	fd_read = open(PIPE_read, O_RDONLY);
@@ -49,7 +74,6 @@ int main(int argc, char** argv) {
 	}
 	printf("SUCCESS");
 
-	for (;;) {
 	size = 0;
 	read(fd_read, &size, 1);
 	char* request = (char*)calloc(size + 1, sizeof(char));
@@ -100,7 +124,7 @@ int main(int argc, char** argv) {
 		}
 		else {
 			ftruncate(fd, size_sh);
-			shared_mem = (volatile char*)mmap(0, sizeof(char),
+			shared_mem = (volatile char*)mmap(0, size_sh,
 			                                  PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 			unsigned int len = strlen(success);
 			write(fd_write, &len, 1);
@@ -115,19 +139,39 @@ int main(int argc, char** argv) {
 		read(fd_read, &val, sizeof(val));
 
 		write(fd_write, &size, 1);
+
 		for (int i = 0; i < size; i++) {
-			write(fd_write, &request[i], sizeof(char));
+			write(fd_write, &write_to_shm[i], sizeof(char));
 		}
 
-		if (offset >= 0 && offset < size_sh) {
-			shared_mem[offset-1] = val;
-			unsigned int len = strlen(success);
-			write(fd_write, &len, 1);
-			for (int i = 0; i < len; i++) {
-				write(fd_write, &success[i], sizeof(char));
+		if (offset >= 0 && offset < ShSIZE) {
+			unsigned int len = 0;
+
+			char* converted = int_to_string(val);
+			len = strlen(converted);
+			if (offset + len < ShSIZE) {
+				/*int j = offset;
+				for (int i = strlen(converted); i >= 0; i--) {
+					shared_mem[j] = converted[i];
+					j++;
+				}*/
+
+				shared_mem[offset] = val;
+
+				len = strlen(success);
+				write(fd_write, &len, 1);
+				for (int i = 0; i < len; i++) {
+					write(fd_write, &success[i], sizeof(char));
+				}
+			} else {
+				unsigned int len = strlen(error);
+				write(fd_write, &len, 1);
+				for (int i = 0; i < len; i++) {
+					write(fd_write, &error[i], sizeof(char));
+				}
 			}
-		}
-		else {
+
+		} else {
 			unsigned int len = strlen(error);
 			write(fd_write, &len, 1);
 			for (int i = 0; i < len; i++) {
@@ -136,17 +180,57 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	if(strncmp(request,"MAP_FILE", size) == 0){
-		unsigned int file_size =0;
-		read(fd_read,&file_size, 1 );
 
-		file_name = malloc(file_size*sizeof(char));
-		
+
+
+	if (strncmp(request, "MAP_FILE", size) == 0) {
+		unsigned int file_name_size = 0;
+		read(fd_read, &file_name_size, 1);
+		unsigned int len = 0;
+		file_name = malloc(file_name_size * sizeof(char));
+		file_name[(unsigned int)file_name_size]='\0';
+		int fd = -1;
+		fd = open(file_name, O_RDONLY);
+
+		write(fd_write, &size, 1);
+
+		for (int i = 0; i < size; i++) {
+			write(fd_write, &map_file[i], sizeof(char));
+		}
+
+		if (fd == -1) {
+			len = strlen(error);
+			write(fd_write, &len, 1);
+			for (int i = 0; i < len; i++) {
+				write(fd_write, &error[i], sizeof(char));
+			}
+		} else {
+			off_t sz = lseek(fd, 0, SEEK_END);
+			lseek(fd, 0, SEEK_SET);
+			shared_mem = (volatile char*)mmap(NULL, sz, PROT_READ, MAP_SHARED, fd, 0);
+			/*if (shared_mem == (void*) - 1) {
+				len = strlen(error);
+				write(fd_write, &len, 1);
+				for (int i = 0; i < len; i++) {
+					write(fd_write, &error[i], sizeof(char));
+				}
+			} else {*/
+				len = strlen(success);
+				write(fd_write, &len, 1);
+				for (int i = 0; i < len; i++) {
+					write(fd_write, &success[i], sizeof(char));
+				}
+			//}
+		}
+
+
 	}
-
-
+	munmap((void*)shared_mem, size_sh);
+	shared_mem = NULL;
 	free(request);
-	}
+	free(file_name);
+
+
 
 
 	return 0;
